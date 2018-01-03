1ff907b43b57bdcbabf931dc090dc5a31fb684b7
ryanbmarx <ryanbmarx@gmail.com>
2018-01-02 19:59:32 -0600

    Rejigger products page so it is divided by any existing subcategories. As new categories are created, they should end up here, too.

--- category-norwex-products.php
+++ category-norwex-products.php
@@ -12,14 +12,51 @@
 					// Add global shop banner. The banner is edited in the Divi library
 					echo do_shortcode("[showlibraryitem id=27382]"); 
 				?>
-		<?php
-			if ( have_posts() ) :
-				while ( have_posts() ) : the_post();
-					$post_format = et_pb_post_format(); ?>
-
-					<article id="post-<?php the_ID(); ?>" <?php post_class( 'et_pb_post' ); ?>>
-
+				<div class='products'>
+				<div class='products__traveler'>
+					<p class='products__traveler-note'>Choose a product type:</p>
+					<?php 
+						// Load the category list and make the traveler
+						$categories =  get_categories('child_of=125');
+						echo '<ul>';
+						foreach  ($categories as $category) {
+						  	$list_item_string = "<li><a href='#%s'>%s</a></li>";
+							echo sprintf($list_item_string, $category->slug, $category->cat_name);
+						}
+						echo '</ul>';
+					?>
+				</div>
+				<div class='products__posts'>
 				<?php
+					foreach  ($categories as $category) {
+						echo "<section class='product-category' id='" . $category->slug . "'>";
+						echo "<h2 class='product-category__header'>" . $category->cat_name . "</h2>";
+						$args = array(
+							'posts_per_page'   => 10,
+							'offset'           => 0,
+							'category'         => $category->term_id,
+							'category_name'    => '',
+							'orderby'          => 'date',
+							'order'            => 'DESC',
+							'include'          => '',
+							'exclude'          => '',
+							'meta_key'         => '',
+							'meta_value'       => '',
+							'post_type'        => 'post',
+							'post_mime_type'   => '',
+							'post_parent'      => '',
+							'author'	   => '',
+							'author_name'	   => '',
+							'post_status'      => 'publish',
+							'suppress_filters' => true 
+						);
+						$posts_array = get_posts( $args );
+
+
+						foreach ($posts_array as $post ) : setup_postdata($post); ?>
+				
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'et_pb_post' ); ?>>
+<?php
 					$thumb = '';
 
 					$width = (int) apply_filters( 'et_pb_index_blog_image_width', 1080 );
@@ -52,11 +89,11 @@
 
 				<?php if ( ! in_array( $post_format, array( 'link', 'audio', 'quote' ) ) ) : ?>
 					<?php if ( ! in_array( $post_format, array( 'link', 'audio' ) ) ) : ?>
-						<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+						<h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 					<?php endif; ?>
 
 					<?php
-						et_divi_post_meta();
+						// et_divi_post_meta();
 
 						if ( 'on' !== et_get_option( 'divi_blog_style', 'false' ) || ( is_search() && ( 'on' === get_post_meta( get_the_ID(), '_et_pb_use_builder', true ) ) ) ) {
 							truncate_post( 270 );
@@ -65,19 +102,15 @@
 						}
 					?>
 				<?php endif; ?>
+							</article>
+						<?php endforeach; // End posts loop ?>
 
-					</article> <!-- .et_pb_post -->
-			<?php
-					endwhile;
+					<?php echo "<a class='product-category__more' href='/category/" . $category->slug . "'>More " . $category->cat_name . "</a>";?>
 
-					if ( function_exists( 'wp_pagenavi' ) )
-						wp_pagenavi();
-					else
-						get_template_part( 'includes/navigation', 'index' );
-				else :
-					get_template_part( 'includes/no-results', 'index' );
-				endif;
-			?>
+				<?php echo "</section> <!-- End category ".  $category->slug . " section  -->" ?>
+			<?php } // End category loop ?>
+					</div> <!-- CLOSE PRODUCTS__POSTS -->
+				</div> <!-- CLOSE PRODUCTS -->
 			</div> <!-- #left-area -->
 
 			<?php get_sidebar(); ?>
